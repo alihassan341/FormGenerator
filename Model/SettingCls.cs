f36d0ae325e71f81ecbc603688e5d686696c65ba
qusain <qusainqassmi123@gmail.com>
2024-03-13 05:36:01 +0500

    more update and new working

---
+++ Model/SettingCls.cs
@@ -0,0 +1,16 @@
+﻿namespace FormGenerator.Model
+{
+    public class Panel
+    {
+        public string? id { get; set; }
+        public string? title{ get; set; }
+        public string? content { get; set; }
+        public string? placeholder { get; set; }
+        public int? width { get; set; }
+        public bool? trim { get; set; }
+        public bool? required { get; set; }
+        public bool? disabled { get; set; }
+        public bool? clearable { get; set; }
+
+    }
+}
