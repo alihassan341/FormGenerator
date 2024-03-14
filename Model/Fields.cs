1608521cd0ece9ec27bb9cb6aa490788caa83a9d
alihassan341 <aa7671951@gmail.com>
2024-03-14 05:34:17 +0500

    Worked for some fileds

--- Model/Fields.cs
+++ Model/Fields.cs
@@ -2,6 +2,8 @@
 {
     public class InputModel
     {
+        private IList<InputModel> options;
+
         public string Id { get; set; } = "";
         public string Name { get; set; } = "";
         public string Value { get; set; } = "";
@@ -9,6 +11,10 @@
         public string? Class { get; set; } = "";
         public bool Actions { get; set; } = true;
         public string? FieldType { get; set; } = "";
+        public int min { get; set; } = 0;
+        public int step { get; set; } = 1;
+        public int max { get; set; } = 100;
+
     }
 
 }
