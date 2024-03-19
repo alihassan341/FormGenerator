9ffa3b98cc70c88f4784c1666311d0bc3a853fb6
alihassan341 <aa7671951@gmail.com>
2024-03-19 08:27:12 +0500

    Half Work Done  for Ganarate Json

--- Model/Fields.cs
+++ Model/Fields.cs
@@ -1,4 +1,6 @@
-﻿namespace FormGenerator.Model
+﻿using System.Text.Json.Serialization;
+
+namespace FormGenerator.Model
 {
     public class InputModel
     {
@@ -15,6 +17,76 @@
         public int step { get; set; } = 1;
         public int max { get; set; } = 100;
 
+        [JsonPropertyName("type")]
+        public string Types { get; set; } = "input";
+
+        [JsonPropertyName("label")]
+        public string Label { get; set; } = "Cellphone";
+
+        [JsonPropertyName("icon")]
+        public string Icon { get; set; } = "cellphone";
+
+        [JsonPropertyName("key")]
+        public string Key { get; set; } = "input_KwA-nxsnczH0t3OM170V2";
+
+        [JsonPropertyName("id")]
+        public string Ids { get; set; } = "KwA-nxsnczH0t3OM170V2";
+
+        [JsonPropertyName("options")]
+        public InputOptions Options { get; set; } = new InputOptions();
+
+        [JsonPropertyName("style")]
+        public InputStyle Style { get; set; } = new InputStyle();
     }
 
+        public class InputOptions
+        {
+            [JsonPropertyName("clearable")]
+            public bool Clearable { get; set; } = true;
+
+            [JsonPropertyName("renderType")]
+            public int RenderType { get; set; } = 4;
+
+            [JsonPropertyName("disabled")]
+            public bool Disabled { get; set; } = false;
+
+            [JsonPropertyName("showPassword")]
+            public bool ShowPassword { get; set; } = false;
+
+            [JsonPropertyName("defaultValue")]
+            public string DefaultValue { get; set; } = "";
+
+            [JsonPropertyName("placeholder")]
+            public string Placeholder { get; set; } = "Please enter";
+
+            [JsonPropertyName("labelWidth")]
+            public int LabelWidth { get; set; } = 105;
+
+            [JsonPropertyName("isShowLabel")]
+            public bool IsShowLabel { get; set; } = true;
+
+            [JsonPropertyName("required")]
+            public bool Required { get; set; } = true;
+
+            [JsonPropertyName("isShowTrim")]
+            public bool IsShowTrim { get; set; } = true;
+        }
+
+        public class InputStyle
+        {
+            [JsonPropertyName("width")]
+            public WidthStyle Width { get; set; } = new WidthStyle();
+        }
+
+        public class WidthStyle
+    {
+        [JsonPropertyName("pc")]
+        public string PC { get; set; } = "50%";
+
+        [JsonPropertyName("mobile")]
+        public string Mobile { get; set; } = "100%";
+    }
+    
 }
+
+
